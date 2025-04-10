5cb80259624f362d411ea25b925348766d33190d
Pedro-Jordao <phjordao2522@gmail.com>
2025-04-10 17:54:39 -0300

    commit z

--- src/main/java/br/com/dio/barbershopui/entity/ClientEntity.java
+++ src/main/java/br/com/dio/barbershopui/entity/ClientEntity.java
@@ -1,10 +1,14 @@
 package br.com.dio.barbershopui.entity;
 
 import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.GeneratedValue;
+
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
